133c72b2bf31c344d4ea6b9a05fe148aa5d51b33
maxkman <max.ka@gmx.de>
2022-04-28 14:30:13 +0000

    Added plot_layout_mm

---
+++ R/plot_layout_mm.R
@@ -0,0 +1,16 @@
+#' Wrapper for plot_layout with mm as default
+#'
+#' @param width Plot width in mm
+#' @param height Plot height in mm
+#' @param ... Parameters passed on to patchwork::plot_layout
+#'
+#' @return A patchwork
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' p <- ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+#' p + plot_layout_mm(30, 30)
+plot_layout_mm <- function(width, height, ...) {
+  patchwork::plot_layout(..., widths = grid::unit(width, "mm"), heights = grid::unit(height, "mm"))
+}
