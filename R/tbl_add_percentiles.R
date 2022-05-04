c7b10de47287b292ebc2cec0c5e8cc4648de9504
Max Kaufmann <maxkaufmann@max-ka-macnote.zmnh.uni-hamburg.de>
2022-05-04 16:17:47 +0200

    Added tbl_add_percentiles function

---
+++ R/tbl_add_percentiles.R
@@ -0,0 +1,20 @@
+#' Add percentile column to tibble
+#'
+#' @description Adds a new column to the tibble, which shows the percentiles for a column of choice
+#' @param tbl_x A tibble
+#' @param var_x The tibble column for which to calculate percentiles
+#'
+#' @return A tibble
+#' @export
+#'
+#' @examples
+#' tbl_add_percentiles(mtcars, disp)
+tbl_add_percentiles <- function(tbl_x, var_x) {
+  tbl_x %>%
+    mutate(id_tmp = row_number()) %>%
+    arrange({{var_x}}) %>%
+    mutate(rank_tmp = row_number(),
+           "{{var_x}}_percentiles" := rank_tmp / length(rank_tmp) * 100) %>%
+    arrange(id_tmp) %>%
+    select(-id_tmp, -rank_tmp)
+}
