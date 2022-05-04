68be967ad482283d5ac17c0e40fcc13f19ab8b4a
Max Kaufmann <maxkaufmann@max-ka-macnote.zmnh.uni-hamburg.de>
2022-05-04 14:54:55 +0200

    Added my costum ggplot theme

---
+++ R/theme_mk.R
@@ -0,0 +1,14 @@
+theme_mk <- theme_bw() + theme(
+  text = element_text(size = 7),
+  axis.text.x = element_text(size = 6),
+  axis.text.y = element_text(size = 6),
+  legend.text = element_text(size = 7),
+  strip.text = element_text(size = 7),
+  plot.title = element_text(hjust = 0.5, size = 7),
+  legend.key.size = unit(2.5, "mm"),
+  strip.background = element_blank(),
+  line = element_line(size = 0.3),
+  legend.background=element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank()
+)
