49567489b984e3c7dcbd5b3010611e38a1aaf7a4
Max Kaufmann <maxkaufmann@max-ka-macnote.zmnh.uni-hamburg.de>
2022-05-04 16:31:46 +0200

    Added apply_quantile_cutoffs function

---
+++ R/apply_quantile_cutoffs.R
@@ -0,0 +1,24 @@
+#' Applying quantile cutoffs to a numeric vector
+#'
+#' @description Elements in the vector <= the lower cutoff will be set to the lower cutoff.
+#' @description Elements in the vector >= the upper cutoff will be set to the upper cutoff.
+#' @description All other elements will be left unchanged
+#' @param x A numeric vector
+#' @param quantiles Lower and upper quantiles to apply
+#'
+#' @return A numeric vector
+#' @export
+#'
+#' @examples
+#' mtcars$disp %>% sort
+#' apply_quantile_cutoffs(mtcars$disp) %>% sort
+apply_quantile_cutoffs <- function(x, quantiles = c(0.1, 0.9)) {
+  quant_lower <- quantile(x, quantiles[[1]])
+  quant_higher <- quantile(x, quantiles[[2]])
+  x <- if_else(x <= quant_lower,
+          quant_lower,
+          if_else(x >= quant_higher,
+                  quant_higher,
+                  x))
+  unname(x)
+}
