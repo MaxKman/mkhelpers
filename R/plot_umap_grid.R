ebff6dfcfa7bb32f7ce0ffe7e0cc1d0b8734c016
Max Kaufmann <maxkaufmann@max-ka-macnote.zmnh.uni-hamburg.de>
2022-06-09 15:44:01 +0200

    added show_legend_list feature

--- R/plot_umap_grid.R
+++ R/plot_umap_grid.R
@@ -7,6 +7,7 @@
 #' @param out_width Width of image output in mm
 #' @param output_path Output path and file name (should end in .png)
 #' @param show_labels Whether to label the mean coordinates of discrete feature values. Set to TRUE or FALSE to affect all plots or give a list of booleans for all features
+#' @param show_legend Whether to show the ggplot legend. Set to TRUE or FALSE to affect all plots or give a list of booleans for all features.
 #' @param ... Other parameters passed on to plot_umap. Note, that umap_dim_col_1 and umap_dim_col_2 need to be provided.
 #'
 #' @return
@@ -29,7 +30,7 @@
 #'    expr_format = "wide")
 #' plot_umap_grid(tbl_x = tbl_x, umap_dim_col_1 = UMAP_1, umap_dim_col_2 = UMAP_2, output_path = "~/test_plot_umap_grid.png", feature_list = c("seurat_clusters", "CD3D", "CD19"), show_labels = TRUE, point_size = 0.5)
 #' }
-plot_umap_grid <- function(tbl_x, feature_list, quantile_limits_list = NULL, n_cols = 3, out_width = 180, output_path, show_labels = FALSE, ...) {
+plot_umap_grid <- function(tbl_x, feature_list, quantile_limits_list = NULL, n_cols = 3, out_width = 180, output_path, show_labels = FALSE, show_legend = TRUE, ...) {
   n_plots <- length(feature_list)
   col_width <- out_width / n_cols
   out_height <- ceiling(n_plots / n_cols) * col_width * 0.9
@@ -44,9 +45,16 @@ plot_umap_grid <- function(tbl_x, feature_list, quantile_limits_list = NULL, n_c
   } else {
     show_labels_list <- show_labels
   }
+  if(length(show_legend) == 1 & show_legend) {
+    show_legend_list <- rep(TRUE, length(feature_list))
+  } else if(length(show_labels) == 1 & !show_legend) {
+    show_legend_list <- rep(FALSE, length(feature_list))
+  } else {
+    show_legend_list <- show_legend
+  }
 
-  plot_l <- pmap(list(feature_list, quantile_limits_list, show_labels_list), function(feature_x, quantile_limits_x, show_labels_x) {
-    plot_umap(tbl_x = tbl_x, feature_x = !!sym(feature_x), plot_width = 0.6 * col_width, plot_height = 0.6 * col_width, output = "plot", title = feature_x, quantile_limits = quantile_limits_x, show_labels = show_labels_x, ...)
+  plot_l <- pmap(list(feature_list, quantile_limits_list, show_labels_list, show_legend_list), function(feature_x, quantile_limits_x, show_labels_x, show_legend_x) {
+    plot_umap(tbl_x = tbl_x, feature_x = !!sym(feature_x), plot_width = 0.6 * col_width, plot_height = 0.6 * col_width, output = "plot", title = feature_x, quantile_limits = quantile_limits_x, show_labels = show_labels_x, show_legend = show_legend_x, ...)
   })
   p <- patchwork::wrap_plots(plot_l, ncol = n_cols, widths = col_width)
   png_save_show(p, output_path, width = out_width, height = out_height)
