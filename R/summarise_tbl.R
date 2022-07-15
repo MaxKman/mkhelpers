fdfccda10661cee2011e257491362aa8e62e88dd
Max Kaufmann <maxkaufmann@max-ka-macnote.zmnh.uni-hamburg.de>
2022-07-15 12:33:34 +0200

    Increased default number of top values to show

--- R/summarise_tbl.R
+++ R/summarise_tbl.R
@@ -2,20 +2,20 @@
 #'
 #' @param tbl_x A tibble
 #'
-#' @return A tibble containing the data types of all columns of the input tibble, the number of non-na values, the number of unique values, the top three values, three randomly drawn examples (cases 1-3, corresponding to entire rows of the original tibble) and one randomly drawn non-na value for each column.
+#' @return A tibble containing the data types of all columns of the input tibble, the number of non-na values, the number of unique values, the top ten values, three randomly drawn examples (cases 1-3, corresponding to entire rows of the original tibble) and one randomly drawn non-na value for each column.
 #' @export
 #'
 #' @examples
-#' summarise_data_types(mtcars)
+#' summarise_tbl(mtcars)
 summarise_tbl <- function(tbl_x) {
-  n_unique_values <- tbl_x %>% map(~unique(.) %>% length)
+  n_unique_values <- tbl_x %>% map(~unique(.) %>% length) %>% unlist
   top_3_values <- colnames(tbl_x) %>%
     map(~count_(tbl_x,.) %>%
-          slice_max(order_by = n, n = 3, with_ties = FALSE) %>%
+          slice_max(order_by = n, n = 10, with_ties = FALSE) %>%
           pull(1) %>%
           as.character %>%
           str_replace_na %>%
-          str_c(collapse = ", ")) %>%
+          str_c(collapse = "; ")) %>%
     unlist
   # The as.character conversion needs to be done for each column separately
   # otherwise errors are easily introduced, e.g. for dates
