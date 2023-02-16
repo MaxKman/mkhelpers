60a2ee379f8fb4659c85ccfca7a3cc425a066dbc
max-ka <m.kaufmann@berkeley.edu>
2023-02-16 17:19:27 +0100

    And another bug fix for lfc cutoff

--- R/DGE_analysis.R
+++ R/DGE_analysis.R
@@ -203,7 +203,7 @@ DGE_analysis <- function(m, md, m_norm, cluster_col, sample_col, group_col, batc
         pivot_wider(names_from = {{group_col}}, values_from = mean_exp)
 
       log2fc <- aggr_exp %>%
-        mutate(log2fc = log2(!!sym(group2) / !!sym(group2)))
+        mutate(log2fc = log2(!!sym(group2) / !!sym(group1)))
       View(log2fc)
       log2fc <- log2fc %>%
         filter(is.finite(log2fc) & (abs(log2fc) >= lfc_threshold))
