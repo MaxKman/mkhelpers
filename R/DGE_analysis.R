a1920504d9892ad47dda2c1fa953f53c074889a7
max-ka <m.kaufmann@berkeley.edu>
2023-02-23 13:43:14 +0100

    Adapted function for more than two groups

--- R/DGE_analysis.R
+++ R/DGE_analysis.R
@@ -9,8 +9,7 @@
 #' @param balance_batches If set to TRUE, will ensure that only samples are included in the comparison for which a corresponding sample from the same batch is available in the other group. Requires batch_col to be defined.
 #' @param add_var An additional column that should be preserved, for example because it is used in the design formula
 #' @param title The title of comparison (will be used for file naming)
-#' @param group1 The name of group 1 in group_col
-#' @param group2 The name of group 2 in group_col
+#' @param groups The name of the experimental groups, e.g. c('group_1', 'group2'). The order of groups will be used as factor levels.
 #' @param design The DEseq2 design formula to be used
 #' @param LRT Whether to let DEseq2 perform a likelihood ratio test instead of a Wald test. Default: False
 #' @param reduced_design Reduced model to test against when LRT is set to true
@@ -22,7 +21,7 @@
 #' @param exp_percentage_strict If set to TRUE, only those genes will be included in the DGE analysis, which are expressed in exp_percentage cells in each sample in a cluster from one group. If set to FALSE the inclusion criterion is an average expression in exp_percentage cells across samples (default: FALSE)
 #' @param exp_percentage_type Whether to select genes, which meet the exp_percentage criterion in both groups ('intersect') or in one or both groups ('union'). Default: 'intersect'.
 #' @param aggr_counts_non_zero_percentage This parameter is used to exclude genes from the DGE analysis, for which less than a given percentage of samples have zero expression after count aggregation. This is helpful in stimulation experiments, where some genes are hardly detectable in the unstimulated condition, which can lead to spurious DGE results due to inflated fold changes and low variation. Default: 90%.
-#' @param lfc_threshold Minimum required log2 fold change threshold required for differential gene expression testing. A normalized expression matrix needs to be provided (m_norm).
+#' @param lfc_threshold Minimum log2 fold change threshold required for differential gene expression testing. A normalized expression matrix needs to be provided (m_norm). Fold changes are calculated between the first two groups.
 #' @param save_results Whether to save results to an RDS file and a log file containing the settings used in this run (default: FALSE)
 #' @param save_raw_deseq_objs Save raw DEseq 2 objects, to allow for costum contrast extraction
 #' @param savepath Path where the results of the analysis should be saved
@@ -72,15 +71,17 @@
 #'                          cell_name_col = cell_name,
 #'                          exp_percentage = 1)
 #'  }
-DGE_analysis <- function(m, md, m_norm = NULL, cluster_col, sample_col, group_col, batch_col = NULL, balance_batches = FALSE, add_var = NULL, title, group1, group2, design = ~group, LRT = FALSE, reduced_design = ~1, n_cells_normalize, n_cells_min, min_n_samples_group, cell_name_col = cell_name, exp_percentage = 5, exp_percentage_strict = FALSE, exp_percentage_type = c('intersect', 'union'), aggr_counts_non_zero_percentage = 90, lfc_threshold = 0, save_results = FALSE, save_raw_deseq_objs = FALSE, savepath = "../../RDS/DGE") {
+DGE_analysis <- function(m, md, m_norm = NULL, cluster_col, sample_col, group_col, batch_col = NULL, balance_batches = FALSE, add_var = NULL, title, groups, design = ~group, LRT = FALSE, reduced_design = ~1, n_cells_normalize, n_cells_min, min_n_samples_group, cell_name_col = cell_name, exp_percentage = 5, exp_percentage_strict = FALSE, exp_percentage_type = c('intersect', 'union'), aggr_counts_non_zero_percentage = 90, lfc_threshold = 0, save_results = FALSE, save_raw_deseq_objs = FALSE, savepath = "../../RDS/DGE") {
   sample_col_str <- deparse(substitute(sample_col))
-  md <- md %>% mutate({{group_col}} := factor({{group_col}}, levels = c(group1, group2)))
+  md <- md %>% mutate({{group_col}} := factor({{group_col}}, levels = groups))
 
   md_persample <- md %>% select({{sample_col}}, {{group_col}}, {{add_var}}, {{batch_col}}) %>% distinct() %>% ungroup %>% as.data.frame()
   rownames(md_persample) <- md_persample %>% pull({{sample_col}})
 
-  samples_g1 <- md %>% filter({{group_col}} == group1) %>% pull({{sample_col}}) %>% unique
-  samples_g2 <- md %>% filter({{group_col}} == group2) %>% pull({{sample_col}}) %>% unique
+  group_samples <- map_chr(groups, function(group_x) {
+    md %>% filter({{group_col}} == group_x) %>% pull({{sample_col}}) %>% unique
+  })
+  names(group_samples) <- groups
 
   md <- md %>% group_by({{cluster_col}})
   cells_cluster_sample <- md %>% group_split() %>% map(function(x) x %>% split(.[,sample_col_str])) %>% map_depth(~pull(., {{cell_name_col}}), .depth = 2)
@@ -92,7 +93,7 @@ DGE_analysis <- function(m, md, m_norm = NULL, cluster_col, sample_col, group_co
     x[x %>% map(length) > n_cells_min]
   })
 
-  # ensure that only samples are included in the comparison for which a corresponding sample from the same batch is available in the other group. Requires batch_col to be defined.
+  # ensure that only samples are included in the comparison for which a corresponding sample from the same batch is available in all groups. Requires batch_col to be defined.
   if(balance_batches) {
     for (k in seq_along(cells_cluster_sample)) {
       batch_info <- md %>%
@@ -101,15 +102,15 @@ DGE_analysis <- function(m, md, m_norm = NULL, cluster_col, sample_col, group_co
         distinct %>%
         filter({{sample_col}} %in% names(cells_cluster_sample[[k]]))
       suppressMessages(
-        batches_both_groups <- batch_info %>%
+        batches_all_groups <- batch_info %>%
           group_by({{group_col}}, {{batch_col}}) %>%
           summarise(n_batch = n()) %>%
           pivot_wider(names_from = {{group_col}}, values_from = n_batch) %>%
           drop_na %>%
           left_join(batch_info)
       )
-      donors_keep <- batches_both_groups %>% pull({{sample_col}})
-      if(nrow(batches_both_groups) >= 1) {
+      donors_keep <- batches_all_groups %>% pull({{sample_col}})
+      if(nrow(batches_all_groups) >= 1) {
         cells_cluster_sample[[k]] <- cells_cluster_sample[[k]][donors_keep]
       } else {
         cells_cluster_sample[[k]] <- NA
@@ -119,10 +120,14 @@ DGE_analysis <- function(m, md, m_norm = NULL, cluster_col, sample_col, group_co
 
   # Remove clusters, for which less than min_n_samples_group samples remain in a group
   for (k in seq_along(cells_cluster_sample)) {
-    n_g1 <- names(cells_cluster_sample[[k]]) %in% samples_g1 %>% sum
-    n_g2<- names(cells_cluster_sample[[k]]) %in% samples_g2 %>% sum
-    if(n_g1 < min_n_samples_group || n_g2 < min_n_samples_group) {cells_cluster_sample[[k]] <- NA}
+    insufficient_cells <- map(group_samples, function(group_samples_x) {
+      n_enough <- names(cells_cluster_sample[[k]]) %in% group_samples_x %>% sum
+      n_enough < min_n_samples_group
+    })
+
+    if(any(insufficient_cells)) {cells_cluster_sample[[k]] <- NA}
   }
+
   # Print message to note which samples are kept
   for (k in seq_along(cells_cluster_sample)) {
     if(!is.na(cells_cluster_sample[[k]][[1]][[1]])) {
@@ -145,19 +150,19 @@ DGE_analysis <- function(m, md, m_norm = NULL, cluster_col, sample_col, group_co
     if(exp_percentage_strict) {
       # Subset on genes, which are at least expressed in x% of cells of each sample within a group
       sample_genes <- map(x, ~rownames(.[Matrix::rowSums(. > 0) >= ncol(.)*exp_percentage/100,]))
-      genes_1 <- sample_genes[names(sample_genes) %in% samples_g1] %>% purrr::reduce(.x = ., .f = intersect)
-      genes_2 <- sample_genes[names(sample_genes) %in% samples_g2] %>% purrr::reduce(., intersect)
+      sample_genes <- map(group_samples, function(group_samples_x) {
+        sample_genes[names(sample_genes) %in% group_samples_x] %>% purrr::reduce(intersect)
+      })
       if(exp_percentage_type[[1]] == 'union') {
-        genes_select <- union(genes_1, genes_2)
+        genes_select <- sample_genes %>% purrr:reduce(union)
       } else {
-        genes_select <- intersect(genes_1, genes_2)
+        genes_select <- sample_genes %>% purrr:reduce(intersect)
       }
-
     } else {
       # Subset on genes, which are on average expressed in exp_percentage cells in each group
       rowsumsdf <- map_dfc(x, ~Matrix::rowSums(. > 0) / ncol(.))
-      genes_select <- map(list(samples_g1, samples_g2), function(samples_x) {
-        genes_select <- rowsumsdf[,colnames(rowsumsdf) %in% samples_x] %>% rowMeans()
+      genes_select <- map(group_samples, function(group_samples_x) {
+        genes_select <- rowsumsdf[,colnames(rowsumsdf) %in% group_samples_x] %>% rowMeans()
         names(genes_select) <- rownames(x[[1]])
         genes_select <- genes_select[genes_select >= exp_percentage/100]
         return(names(genes_select))
@@ -187,8 +192,8 @@ DGE_analysis <- function(m, md, m_norm = NULL, cluster_col, sample_col, group_co
     colnames(x) <- cnames
 
     # Select genes, whose aggregated counts are non-zero in > x% of samples
-    genes_select_greater_zero <- map(list(samples_g1, samples_g2), function(samples_x) {
-      greater_zero <- (x[,colnames(x) %in% samples_x] > 0) %>% as.matrix()
+    genes_select_greater_zero <- map(group_samples, function(group_samples_x) {
+      greater_zero <- (x[,colnames(x) %in% group_samples] > 0) %>% as.matrix()
       greater_zero <- rowSums(greater_zero) / ncol(greater_zero)
       greater_zero[greater_zero > aggr_counts_non_zero_percentage / 100] %>% names
     }) %>% purrr::reduce(intersect)
@@ -206,10 +211,11 @@ DGE_analysis <- function(m, md, m_norm = NULL, cluster_col, sample_col, group_co
             left_join(md_persample %>% select({{sample_col}}, {{group_col}}))
         }) %>%
           summarise(mean_exp = mean(exp), .by = c(gene, {{group_col}})) %>%
+          filter({{group_col}} %in% groups[1:2])
           pivot_wider(names_from = {{group_col}}, values_from = mean_exp)
 
         log2fc <- aggr_exp %>%
-          mutate(log2fc = log2(!!sym(group2) / !!sym(group1)))
+          mutate(log2fc = log2(!!sym(groups[[2]]) / !!sym(groups[[1]])))
         log2fc <- log2fc %>%
           filter(is.finite(log2fc) & (abs(log2fc) >= lfc_threshold))
         genes_log2fc <- log2fc %>% pull(gene) %>% unique
@@ -261,14 +267,19 @@ DGE_analysis <- function(m, md, m_norm = NULL, cluster_col, sample_col, group_co
                      "Cluster column: {cluster_col_str}",
                      "Sample column: {sample_col_str}",
                      "Group column: {group_col_str}",
-                     "Group 1: {group1}",
-                     "Group 2: {group2}",
+                     "Groups : {str_c(groups, collapse = ', ')}",
                      "Design: {deparse1(design)}",
+                     "Reduced design (only active if LRT = True): {deparse1(reduced_design)}",
+                     "LRT: {LRT}",
                      "Balance batches: {balance_batches}",
                      "Number of cells to normalize to: {n_cells_normalize}",
                      "Minimum cells per sample: {n_cells_min}",
                      "Minimum number of samples per group: {min_n_samples_group}",
                      "Minimum expression percentage: {exp_percentage}",
+                     "Strict application of expression percentage: {exp_percentage_strict}",
+                     "Expression percentage type: {exp_percentage_type}",
+                     "Aggregation counts non-zero percentage: {aggr_counts_non_zero_percentage}",
+                     "Log2 fold change treshold: {lfc_threshold}",
                      .sep = "\n"))
   }
   return(results)
